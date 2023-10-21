5e36d16b31a9e34d2f9999848d9387536678d92b
Ibrahimov Aydin <80528765+GraveDigger99@users.noreply.github.com>
2023-10-21 12:04:18 +0400

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,12 @@
+(() => {
+    "use strict";
+    let addWindowScrollEvent = false;
+    setTimeout((() => {
+        if (addWindowScrollEvent) {
+            let windowScroll = new Event("windowScroll");
+            window.addEventListener("scroll", (function(e) {
+                document.dispatchEvent(windowScroll);
+            }));
+        }
+    }), 0);
+})();
